26def08adf67af7214ee9ce06caafad73a8fa9f9
Matteo Glock <matteo.glock@numworks.com>
2025-11-04 18:04:06 +0100

    nvim: move lsp/ to after/lsp/ to avoid overrides

--- nvim/after/lsp/ruby_lsp.lua
+++ nvim/after/lsp/ruby_lsp.lua
@@ -1,5 +1,7 @@
+local ruby_lsp_cmd = vim.fn.filereadable("Gemfile") == 1 and { "bundle", "exec", "ruby-lsp" } or { vim.fn.expand("~/.asdf/shims/ruby-lsp") }
+
 return {
-  cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
+  cmd = ruby_lsp_cmd,
   mason = false,
   init_options = {
     addonSettings = {
