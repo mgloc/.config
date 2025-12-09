8c368ea9c1f841cb6fbec6006855233ffe42f79b
mgloc <mglock@outlook.fr>
2025-12-09 15:54:30 +0100

    nvim: git plugins

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,7 +1,13 @@
 vim.pack.add({
+  { src = "https://github.com/kdheepak/lazygit.nvim" },
   { src = "https://github.com/lewis6991/gitsigns.nvim" },
+  { src = "https://github.com/tpope/vim-fugitive" },
 })
 
+-- LazyGit
+vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>")
+
+-- Gitsigns
 require("gitsigns").setup({
   signs = {
     add = { text = "+" },
