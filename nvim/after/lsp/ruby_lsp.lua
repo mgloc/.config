d7e6f62b664453aa2c66da996560db0196d80f1c
mgloc <mglock@outlook.fr>
2025-12-09 15:54:43 +0100

    nvim: better choose between bundle ruby lsp or system

--- nvim/after/lsp/ruby_lsp.lua
+++ nvim/after/lsp/ruby_lsp.lua
@@ -1,4 +1,5 @@
-local ruby_lsp_cmd = vim.fn.filereadable("Gemfile") == 1 and { "bundle", "exec", "ruby-lsp" } or { vim.fn.expand("~/.asdf/shims/ruby-lsp") }
+local has_ruby_lsp = vim.fn.system("bundle show ruby-lsp 2>/dev/null"):match("ruby%-lsp")
+local ruby_lsp_cmd = has_ruby_lsp and { "bundle", "exec", "ruby-lsp" } or { vim.fn.expand("ruby-lsp") }
 
 return {
   cmd = ruby_lsp_cmd,
