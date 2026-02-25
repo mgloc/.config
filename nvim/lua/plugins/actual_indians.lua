bd8ce5ac1c3a71474f4e1ca7f7904074a8c0e7ee
mgloc <mglock@outlook.fr>
2026-02-25 10:51:30 +0100

    nvim: 99 setup

--- nvim/lua/plugins/actual_indians.lua
+++ nvim/lua/plugins/actual_indians.lua
@@ -1,6 +1,7 @@
 vim.pack.add({
   -- { src = "https://github.com/supermaven-inc/supermaven-nvim" },
   { src = "https://github.com/github/copilot.vim" },
+  { src = "https://github.com/ThePrimeagen/99" },
 })
 
 -- Supermaven Setup
@@ -29,3 +30,33 @@ end)
 vim.g.copilot_filetypes = {
   csv = false,
 }
+
+vim.g.copilot_node_command = "mise x node@latest -- node"
+
+-- 99 Setup
+
+local _99 = require("99")
+local cwd = vim.uv.cwd()
+local basename = vim.fs.basename(cwd)
+
+_99.setup({
+  provider = _99.Providers.ClaudeCodeProvider,
+  logger = {
+    level = _99.DEBUG,
+    path = "/tmp/" .. basename .. ".99.debug",
+    print_on_error = true,
+  },
+  tmp_dir = "./tmp",
+})
+
+vim.keymap.set("v", "<leader>aiv", function()
+  _99.visual({})
+end)
+
+vim.keymap.set("n", "<leader>aix", function()
+  _99.stop_all_requests()
+end)
+
+vim.keymap.set("n", "<leader>ais", function()
+  _99.search({})
+end)
