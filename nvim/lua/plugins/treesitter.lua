be1da448049b86bab5160f34ad970d2a52b53a0f
mgloc <mglock@outlook.fr>
2026-03-31 13:50:30 +0200

    nvim: treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,46 @@
 vim.pack.add({
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 })
 
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "query", "vim", "vimdoc", "sql", "javascript", "typescript", "jinja" },
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = { "ruby" },
-  },
-  indent = { enable = true, disable = { "ruby" } },
+local nvim_treesitter = require("nvim-treesitter")
+
+local langs = {
+  "bash",
+  "c",
+  "diff",
+  "html",
+  "lua",
+  "luadoc",
+  "query",
+  "vim",
+  "vimdoc",
+  "sql",
+  "javascript",
+  "typescript",
+  "jinja",
+  "ruby",
+  "yaml",
+}
+
+nvim_treesitter.install(langs)
+
+local parsers = nvim_treesitter.get_installed()
+local filetypes = {}
+for _, lang in ipairs(parsers) do
+  for _, filetype in ipairs(vim.treesitter.language.get_filetypes(lang) or {}) do
+    filetypes[filetype] = true
+  end
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = vim.tbl_keys(filetypes),
+  callback = function(ev)
+    vim.treesitter.start(ev.buf)
+
+    -- Enable legacy vim regex syntax highlighting for ruby
+    local buffer_options = vim.bo[ev.buf]
+    if buffer_options.filetype == "ruby" then
+      buffer_options.syntax = "ON"
+    end
+  end,
 })
