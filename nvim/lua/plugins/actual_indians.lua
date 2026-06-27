a2097e9c5b76d964e9473ac57c5788941e76ed19
mgloc <mglock@outlook.fr>
2026-06-27 10:52:19 +0200

    nvim: remove copilot

--- nvim/lua/plugins/actual_indians.lua
+++ nvim/lua/plugins/actual_indians.lua
@@ -1,6 +1,6 @@
 vim.pack.add({
   -- { src = "https://github.com/supermaven-inc/supermaven-nvim" },
-  { src = "https://github.com/github/copilot.vim" },
+  -- { src = "https://github.com/github/copilot.vim" },
   { src = "https://github.com/ThePrimeagen/99" },
 })
 
