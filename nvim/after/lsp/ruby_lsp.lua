2101791865e3934cd217db954a231fafcac6076e
mgloc <mglock@outlook.fr>
2026-09-21 14:39:57 +0200

    nvim: safer ruby lsp start

--- nvim/after/lsp/ruby_lsp.lua
+++ nvim/after/lsp/ruby_lsp.lua
@@ -2,7 +2,9 @@ local has_ruby_lsp = vim.fn.system("bundle show ruby-lsp 2>/dev/null"):match("ru
 local ruby_lsp_cmd = has_ruby_lsp and { "bundle", "exec", "ruby-lsp" } or { vim.fn.expand("ruby-lsp") }
 
 return {
-  cmd = ruby_lsp_cmd,
+  cmd = function(dispatchers, config)
+    return vim.lsp.rpc.start(ruby_lsp_cmd, dispatchers, config and config.root_dir and { cwd = config.cmd_cwd or config.root_dir })
+  end,
   mason = false,
   init_options = {
     addonSettings = {
